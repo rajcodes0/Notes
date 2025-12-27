b920cf0a9746155f46eb332b1918539fbda77b21
Raj <webamanraj@gmail.com>
2025-12-27 13:42:21 +0530

    maked

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors"
-import path from "path"
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import notesRoutes from "./Routes/notesroutes.js";
 import connectDB from "./config/db.js";
@@ -11,32 +12,30 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const _dirname = path.resolve()
 
-if(process.env.NODE_ENV !== "production"){
-app.use(cors({
-  origin:"http://localhost:5173",
-}))
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
 }
 
-
-app.use(express.json()); 
+app.use(express.json());
 app.use(rate_limiter);
 app.use("/api/notes", notesRoutes);
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(_dirname,"../frontend/dist")))
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-
-app.get("*",(req,res) =>{
-  res.sendFile(path.join(_dirname,"../frontend","dist","index.html"))
-})
+  app.get("/*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "../frontend/dist/index.html")
+    );
+  });
 }
 
-
-
-connectDB().then(() =>{
-app.listen(PORT, () => {
-  console.log("server is running on port,", PORT);
-})
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("server running on port", PORT);
+  });
 });
